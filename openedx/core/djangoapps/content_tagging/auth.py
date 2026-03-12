4600d8890fa9528e5c6867d2b58ab637f7dd9f07
Rodrigo Méndez <rodrigo.mendez@wgu.edu>
2026-03-12 15:25:36 -0600

    feat: New Authz checks on import and export endpoints

--- openedx/core/djangoapps/content_tagging/auth.py
+++ openedx/core/djangoapps/content_tagging/auth.py
@@ -1,12 +1,37 @@
 """
 Functions to validate the access in content tagging actions
 """
+import logging
 
-
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
+from openedx_authz import api as authz_api
+from openedx_authz.constants.permissions import COURSES_EXPORT_TAGS
 from openedx_tagging import rules as oel_tagging_rules
+
+from openedx.core import toggles as core_toggles
+
+log = logging.getLogger(__name__)
 
 
 def has_view_object_tags_access(user, object_id):
+    """
+    Check if the user has access to view object tags for the given object.
+    """
+    # If authz is enabled, check for the export tags authz permission
+    course_key = None
+    try:
+        course_key = CourseKey.from_string(object_id)
+    except InvalidKeyError:
+        log.warning("Invalid course key %s", object_id)
+
+    if course_key and core_toggles.enable_authz_course_authoring(course_key) and not authz_api.is_user_allowed(
+        user.username, COURSES_EXPORT_TAGS.identifier, str(course_key)
+    ):
+        return False
+
+
+    # Always check for tagging permissions
     return user.has_perm(
         "oel_tagging.view_objecttag",
         # The obj arg expects a model, but we are passing an object
