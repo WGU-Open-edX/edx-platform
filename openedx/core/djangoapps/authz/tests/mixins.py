9776759c3e2159ecfd59ff39c42369d9cb3f28d1
Rodrigo Méndez <rodrigo.mendez@wgu.edu>
2026-03-24 11:27:49 -0600

    squash!: Simplify CourseAuthoringAuthzTestMixin change

--- openedx/core/djangoapps/authz/tests/mixins.py
+++ openedx/core/djangoapps/authz/tests/mixins.py
@@ -45,8 +45,8 @@ class CourseAuthoringAuthzTestMixin:
 
         self._seed_policies()
 
-        self.authorized_user = UserFactory(username='authorized', password='test')
-        self.unauthorized_user = UserFactory(username='unauthorized', password='test')
+        self.authorized_user = UserFactory(password=self.password)
+        self.unauthorized_user = UserFactory(password=self.password)
 
         self.authorized_client = APIClient()
         self.authorized_client.force_authenticate(user=self.authorized_user)
